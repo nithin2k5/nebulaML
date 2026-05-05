1d18cb2f305062b0bab4afa8e098942c1dc15cfe
nithin2k5 <ntbm8125@gmail.com>
2026-05-05 13:38:26 +0530

    feat: implement custom glowing cursor matching NebulaML theme

--- client/src/components/ClientProvider.js
+++ client/src/components/ClientProvider.js
@@ -2,10 +2,12 @@
 
 import { AuthProvider } from "@/context/AuthContext";
 import ChatbotWidget from "@/components/ChatbotWidget";
+import CustomCursor from "@/components/CustomCursor";
 
 export default function ClientProvider({ children }) {
   return (
     <AuthProvider>
+      <CustomCursor />
       {children}
       <ChatbotWidget />
     </AuthProvider>
